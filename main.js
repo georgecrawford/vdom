db869ac9f5a3f81a0041ba41df1a2ac0c96836f0
George Crawford <george.crawford@ft.com>
2015-01-28 21:36:24 +0000

    Use UltraDeepClone

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var virtualize = require("vdom-virtualize");
-var h = require("virtual-dom/h");
-var diff = require("virtual-dom/diff");
-var patch = require("virtual-dom/patch");
-var clone = require('./clone');
+var virtualize = require('vdom-virtualize');
+var h = require('virtual-dom/h');
+var diff = require('virtual-dom/diff');
+var patch = require('virtual-dom/patch');
+var clone = require('udc');
 
 document.addEventListener('DOMContentLoaded', function() {
 
