7c29b67c0d55f8045c6515481b1b319e701af398
George Crawford <george.crawford@ft.com>
2015-01-28 21:52:30 +0000

    Use Mustache to render HTML for the new node

--- main.js
+++ main.js
@@ -2,32 +2,39 @@ var virtualize = require('vdom-virtualize');
 var h = require('virtual-dom/h');
 var diff = require('virtual-dom/diff');
 var patch = require('virtual-dom/patch');
-var clone = require('udc');
+var mustache = require('mustache');
+
+var template = '<section style="background-color: #{{color}};"><h2>{{heading}}</h2><p>{{text}}</p></section>';
 
 document.addEventListener('DOMContentLoaded', function() {
 
-	var button = document.querySelector('button');
+	var button  = document.querySelector('button');
 	var section = document.querySelector('section');
+	var heading = section.querySelector('h2').innerText;
+	var text    = section.querySelector('p').innerText;
 
 	// Get a VNode for the initial, rendered DOM node
-	var existingNode = virtualize(section);
-	console.log('Initial DOM node', existingNode);
+	var existingVNode = virtualize(section);
+	console.log('Initial DOM node', existingVNode);
 
 	button.addEventListener('click', function() {
 
-		// This is where FruitMachine would create a new virtual DOM representation. For
-		// now, we just clone the existing node and alter some properties
-		var newNode = clone(existingNode);
-		newNode.properties.style['background-color'] = '#' + (Math.random().toString(16).slice(2, 8));
-		console.log('New DOM node', newNode);
+		// Newly-generated HTML, from fruitmachine's module.toHTML() for example
+		var newHTML = mustache.render(template, {
+			color:   Math.random().toString(16).slice(2, 8),
+			heading: heading,
+			text:    text
+		});
+		var newVNode = virtualize.fromHTML(newHTML);
+		console.log('New DOM node', newVNode);
 
-		var patches = diff(existingNode, newNode);
+		var patches = diff(existingVNode, newVNode);
 		console.log('Applying patches', patches);
 
 		patch(section, patches);
 
 		// Set the pointer to what's now in the DOM
-		existingNode = newNode;
+		existingVNode = newVNode;
 	});
 });
 
