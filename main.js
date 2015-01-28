ab144fd87d3291a2e0014194422017f51676b969
George Crawford <george.crawford@ft.com>
2015-01-28 21:02:00 +0000

    Updated with comments and clone of original node

--- main.js
+++ main.js
@@ -2,21 +2,32 @@ var virtualize = require("vdom-virtualize");
 var h = require("virtual-dom/h");
 var diff = require("virtual-dom/diff");
 var patch = require("virtual-dom/patch");
+var clone = require('./clone');
 
 document.addEventListener('DOMContentLoaded', function() {
+
 	var button = document.querySelector('button');
-	var i = 1;
+	var section = document.querySelector('section');
+
+	// Get a VNode for the initial, rendered DOM node
+	var existingNode = virtualize(section);
+	console.log('Initial DOM node', existingNode);
+
 	button.addEventListener('click', function() {
-		var section = document.querySelector('section');
-		var leftNode = virtualize(section);
-		console.log(leftNode);
 
-		var rightNode = virtualize(section);
-		rightNode.properties.style['background-color'] = '#' + (Math.random().toString(16).slice(2, 8));
+		// This is where FruitMachine would create a new virtual DOM representation. For
+		// now, we just clone the existing node and alter some properties
+		var newNode = clone(existingNode);
+		newNode.properties.style['background-color'] = '#' + (Math.random().toString(16).slice(2, 8));
+		console.log('New DOM node', newNode);
 
-		var patches = diff(leftNode, rightNode);
-		console.log(patches);
+		var patches = diff(existingNode, newNode);
+		console.log('Applying patches', patches);
+
 		patch(section, patches);
+
+		// Set the pointer to what's now in the DOM
+		existingNode = newNode;
 	});
 });
 
