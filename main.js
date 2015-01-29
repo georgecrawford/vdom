5de017dbd4367d491a7ee07d8683651d0107e791
George Crawford <george.crawford@ft.com>
2015-01-29 10:49:23 +0000

    Fastclick

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ var virtualize = require('vdom-virtualize');
 var h = require('virtual-dom/h');
 var diff = require('virtual-dom/diff');
 var patch = require('virtual-dom/patch');
+var attachFastClick = require('fastclick');
 
-var template = '';
 
 document.addEventListener('DOMContentLoaded', function() {
+
+	attachFastClick(document.body);
 
 	var logEl   = document.querySelector('#log');
 	var button  = document.querySelector('button');
